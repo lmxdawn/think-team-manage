cadde3b31eb2b39cf4dbb4ae816fa607304d8224
862253272@qq.com <862253272@qq.com>
2017-01-12 18:40:18 +0800

    增加登录页面

--- public/static/modules/admin/login/js/login-1.0.js
+++ public/static/modules/admin/login/js/login-1.0.js
@@ -13,46 +13,25 @@ layui.use([],function () {
     };
     var lock = false;
     $(function () {
-
-        $('#switch_qlogin').click(function(){
-            $('#switch_login').removeClass("switch_btn_focus").addClass('switch_btn');
-            $('#switch_qlogin').removeClass("switch_btn").addClass('switch_btn_focus');
-            $('#switch_bottom').animate({left:'0px',width:'70px'});
-            $('#qlogin').css('display','none');
-            $('#web_qr_login').css('display','block');
-
-        });
-        $('#switch_login').click(function(){
-
-            $('#switch_login').removeClass("switch_btn").addClass('switch_btn_focus');
-            $('#switch_qlogin').removeClass("switch_btn_focus").addClass('switch_btn');
-            $('#switch_bottom').animate({left:'154px',width:'70px'});
-
-            $('#qlogin').css('display','block');
-            $('#web_qr_login').css('display','none');
-        });
-
-
         $('#login_btn').click(function(){
             if(lock){
                 return;
             }
             lock = true;
-            var $login_err_msg = $("#login_err_msg");
-            $login_err_msg.hide();
+            $('#err_msg').hide();
             var username = $('#username').val();
             var password = $('#password').val();
             var code = $('#code').val();
             if (username == ''){
-                $login_err_msg.show().html("<span style='color:red'>请输入用户名</span>");
+                $('#err_msg').show().html("<span style='color:red'>请输入用户名</span>");
                 return false;
             }
             if (password == ''){
-                $login_err_msg.show().html("<span style='color:red'>请输入密码</span>");
+                $('#err_msg').show().html("<span style='color:red'>请输入密码</span>");
                 return false;
             }
             if (code == ''){
-                $login_err_msg.show().html("<span style='color:red'>请输入验证码</span>");
+                $('#err_msg').show().html("<span style='color:red'>请输入验证码</span>");
                 return false;
             }
 
@@ -62,7 +41,7 @@ layui.use([],function () {
                 lock = false;
                 $('#login_btn').val('登录').removeClass('btn-danger').addClass('btn-success');
                 if(data.code!=1){
-                    $login_err_msg.show().html("<span style='color:red'>"+data.msg+"</span>");
+                    $('#err_msg').show().html("<span style='color:red'>"+data.msg+"</span>");
                     return false;
                 }else{
                     window.location.href=data.data;
